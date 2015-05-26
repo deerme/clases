f121972f8c020853437f308f251e2837a1a32c57
Jose Naranjo <naranjja@gmail.com>
2015-05-26 08:50:16 -0500

    Update Ejercicio 13 (Producto de dígitos).sql

--- Ingeniería de Datos/Ejercicio 13 (Producto de dígitos).sql
+++ Ingeniería de Datos/Ejercicio 13 (Producto de dígitos).sql
@@ -13,6 +13,7 @@ WHILE (@N != 0)
 PRINT STR(@PD)
 
 -- Hallar el producto consecutivo de los dígitos de 12345
+DECLARE @N INTEGER, @D INTEGER, @PD INTEGER
 SET @N = 12345
 SET @PD = 1
 
@@ -21,12 +22,14 @@ WHILE (@N != 0)
 		SET @D = @N % 10
 		SET @PD = @PD * @D
 		SET @N = @N / 10
-	END
-
-WHILE (@PD > 10)
-	BEGIN
-		SET @N = @PD
-		CONTINUE -- Continuar loop anterior
+		IF (@PD >= 10)
+			SET @N = @PD
+			WHILE (@N != 0)
+				BEGIN
+					SET @D = @N % 10
+					SET @PD = @PD * @D
+					SET @N = @N / 10
+				END
 	END
 
 PRINT STR(@PD)
