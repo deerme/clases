6618c51ac3aaea0f6e86cc326a3413b4bc03d495
Jose Naranjo <naranjja@gmail.com>
2015-10-29 09:07:40 -0500

    Update Ejercicio 8 (Clusterización con k-means).r

--- Introducción a la Analítica/R/Ejercicio 8 (Clusterización con k-means).r
+++ Introducción a la Analítica/R/Ejercicio 8 (Clusterización con k-means).r
@@ -1,12 +1,12 @@
-irisTemp<-iris
-irisTemp[1:10,]
-irisTemp$Species<-NULL
-irisTemp[1:10,]
-kmeans.res<-kmeans(irisTemp,3)
-table(iris$Species,kmeans.res$cluster)
-col<-kmeans.res$cluster
-col
+irisTemp = iris
+irisTemp$Species = NULL
+
+kmeans.res = kmeans(irisTemp,3)
+table(iris$Species, kmeans.res$cluster)
+
+col = kmeans.res$cluster
+
 c("Sepal.Length","Sepal.Width")
 irisTemp[c("Sepal.Length","Sepal.Width")]
 plot(irisTemp[c("Sepal.Length","Sepal.Width")],col=kmeans.res$cluster)
-points(kmeans.res$centers[,c("Sepal.Length","Sepal.Width")],col=1:3,pch=8,cex=2)
+points(kmeans.res$centers[,c("Sepal.Length","Sepal.Width")], col = 1:3, pch = 8, cex = 2)
